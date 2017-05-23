3616900c378bdc78975b5c1da8b47ec78e38b0ff
zj <zj_donkey@163.com>
2017-05-23 11:24:27 +0800

    注释掉数据库信息

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -9,11 +9,11 @@ return array(
 	/* 数据库配置 */
     'DB_TYPE'   => 'mysql',         // 数据库类型
     'DB_HOST'   => '127.0.0.1',  // 服务器地址
-    'DB_NAME'   => 'huaguan',           // 数据库名
+    'DB_NAME'   => '*',           // 数据库名
     'DB_USER'   => 'root',          // 用户名
-    'DB_PWD'    => 'tydba1212',    // 密码
+    'DB_PWD'    => '*',    // 密码
     'DB_PORT'   => '3306',          // 端口
-    'DB_PREFIX' => 'car_',  
+    'DB_PREFIX' => '*',  
     
 'WX_APPID' => 'wx5172834da314ceb1',
 
